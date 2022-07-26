b458a5b9a52f1b32c5261e9890f6cd112152da47
degD <108623049+degD@users.noreply.github.com>
2022-07-26 09:13:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,36 @@
 from tkinter import *
 from tkinter import ttk
 
+
+class PiePixelPainter():
+    
+    def __init__(self, root):
+        root.title('PIE PIXEL PAINTER')
+        root.option_add('*tearOff', False)
+        
+        menubar = Menu(root) # Menu bar
+        root['menu'] = menubar
+        
+        menu_file = Menu(menubar)
+        menubar.add_cascade(menu=menu_file, label='File')
+        menu_recent = Menu(menu_file)
+        menu_file.add_command(label='New')
+        menu_file.add_command(label='Open...')
+        menu_file.add_cascade(menu=menu_recent, label='Recent Files')
+        menu_file.add_separator()
+        menu_file.add_command(label='Save')
+        menu_file.add_command(label='Save As...')
+        menu_file.add_separator()
+        menu_file.add_command(label='Exit')  
+        
+        menu_edit = Menu(menubar)   
+        menubar.add_cascade(menu=menu_edit, label='Edit')
+        menu_edit.add_command(label='Undo')
+        menu_edit.add_command(label='Redo')
+        menu_edit.add_separator()
+        menu_edit.add_command(label='Options...')
+        
+
 root = Tk()
-root.title('test window')
-
-mainframe = ttk.Frame(root, height=100, width=10)
-
+PiePixelPainter(root)
 root.mainloop()
