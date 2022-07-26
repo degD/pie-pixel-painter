780e4fe66ec35ce1ccc5e6f3dcaf47cbacb5d749
degD <108623049+degD@users.noreply.github.com>
2022-07-26 10:45:56 +0300

    Update main.py
    Working on the canvas...

--- main.py
+++ main.py
@@ -3,6 +3,45 @@ from tkinter import *
 from tkinter import ttk
 
 
+class PixelPainterCanvas(Canvas):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        
+        self.bind("<Button-1>", self.paintsinglepixel)
+        self.bind("<B1-Motion>", self.paintpixels)
+        
+        self.x = 0
+        self.y = 0
+    
+    def paintsinglepixel(self, event):
+        self.x, self.y = self.canvasx(event.x), self.canvasy(event.y)
+        
+        coords = self.pixelcalc(self.x, self.y)
+        self.create_rectangle(*coords, fill='red')
+        
+        
+        
+        
+    def paintpixels(self, event):
+        newx, newy = self.canvasx(event.x), self.canvasy(event.y)
+        
+        self.create_line(self.x, self.y, newx, newy, width=20)
+        self.x, self.y = newx, newy
+        
+    def pixelcalc(self, x, y):
+        print(x, y)
+        size = 20
+        
+        px = x // size
+        py = y // size
+        
+        topx, topy = px*20, py*20
+        bottomx, bottomy = (px+1)*20, (py+1)*20
+        
+        print(topx, topy, bottomx, bottomy)
+        return (topx, topy, bottomx, bottomy)
+
+
 class PiePixelPainter():
     
     def __init__(self, root):
@@ -30,6 +69,24 @@ class PiePixelPainter():
         menu_edit.add_command(label='Redo')
         menu_edit.add_separator()
         menu_edit.add_command(label='Options...')
+                
+        h_scroll = ttk.Scrollbar(root, orient=HORIZONTAL)
+        v_scroll = ttk.Scrollbar(root, orient=VERTICAL)
+        canvas = PixelPainterCanvas(root, scrollregion=(0, 0, 1000, 1000), yscrollcommand=v_scroll.set, xscrollcommand=h_scroll.set)
+        h_scroll['command'] = canvas.xview
+        v_scroll['command'] = canvas.yview
+        
+        canvas.grid(column=0, row=0, sticky=(N, S, W, E))
+        h_scroll.grid(column=0, row=1, sticky=(W, E))
+        v_scroll.grid(column=1, row=0, sticky=(N, S))
+        root.grid_columnconfigure(0, weight=1)
+        root.grid_rowconfigure(0, weight=1)
+        
+        self.x = 0
+        self.y = 0
+        
+        
+        
         
 
 root = Tk()
