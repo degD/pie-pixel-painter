a4d427b5a29f3f0e76f38941e763f9566959067a
degD <108623049+degD@users.noreply.github.com>
2022-07-26 14:44:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,34 @@ class PixelPaintingCanvas(Canvas):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
         
+        self.paintedcoordsid = []
+        self.paintcolor = 'black'
+        
         self.bind("<Button-1>", self.paintsinglepixel)
         self.bind("<B1-Motion>", self.paintpixels)
+        
+    def setcolor(self, color):
+        self.paintcolor = color
+        
+    def erase(self):
+        x, y = self.canvasx(event.x), self.canvasy(event.y)
+        
+        rect_coords = self.pixelcalc(x, y)
+        self.delete()
     
     def paintsinglepixel(self, event):
         x, y = self.canvasx(event.x), self.canvasy(event.y)
         
         rect_coords = self.pixelcalc(x, y)
-        self.create_rectangle(rect_coords, fill='red', outline='red')
+        id = self.create_rectangle(rect_coords, fill=self.paintcolor, outline=self.paintcolor)
+        self.paintedcoordsid.append(id)
          
     def paintpixels(self, event):
         x, y = self.canvasx(event.x), self.canvasy(event.y)
         
         rect_coords = self.pixelcalc(x, y)
-        self.create_rectangle(rect_coords, fill='red', outline='red')
+        id = self.create_rectangle(rect_coords, fill=self.paintcolor, outline=self.paintcolor)
+        self.paintedcoordsid.append(id)
         
     def pixelcalc(self, x, y):
         size = 20
@@ -61,21 +75,38 @@ class PiePixelPainter():
         menu_edit.add_command(label='Redo')
         menu_edit.add_separator()
         menu_edit.add_command(label='Options...')
+        
+        interface = ttk.Frame(root)
+        interface.grid(column=0, row=0, sticky=(N, S, W, E))
+        interface['padding'] = 5
+        
+        colorselectorstyle = ttk.Style()
+        colorselectorstyle.configure('ColorSelector.TFrame', background='red', relief='sunken')
+        colorselector = ttk.Frame(interface, height=30, width=30, style='ColorSelector.TFrame')
+        colorselector.grid(column=0, row=0, sticky=W)
+        ttk.Label(interface, text='color').grid(column=0, row=1, sticky=(W, N, S))
+        
+        eraserselectorstyle = ttk.Style()
+        eraserselectorstyle.configure('EraserSelector.TFrame', background='white', relief='sunken')
+        eraserselector = ttk.Frame(interface, height=30, width=30, style='EraserSelector.TFrame')
+        eraserselector.grid(column=1, row=0, sticky=W)
+        eraserselector.bind('<Button-1>', lambda e: self.canvas.setcolor('white'))
+        ttk.Label(interface, text='erase').grid(column=1, row=1, sticky=(W, N, S))
+        
                 
         h_scroll = ttk.Scrollbar(root, orient=HORIZONTAL)
         v_scroll = ttk.Scrollbar(root, orient=VERTICAL)
-        canvas = PixelPaintingCanvas(root, scrollregion=(0, 0, 1000, 1000), yscrollcommand=v_scroll.set, xscrollcommand=h_scroll.set, background='white')
-        h_scroll['command'] = canvas.xview
-        v_scroll['command'] = canvas.yview
+        self.canvas = PixelPaintingCanvas(root, scrollregion=(0, 0, 1000, 1000), yscrollcommand=v_scroll.set, xscrollcommand=h_scroll.set, background='white')
+        h_scroll['command'] = self.canvas.xview
+        v_scroll['command'] = self.canvas.yview
         
-        canvas.grid(column=0, row=0, sticky=(N, S, W, E))
-        h_scroll.grid(column=0, row=1, sticky=(W, E))
-        v_scroll.grid(column=1, row=0, sticky=(N, S))
+        self.canvas.grid(column=0, row=1, sticky=(N, S, W, E))
+        h_scroll.grid(column=0, row=2, sticky=(W, E))
+        v_scroll.grid(column=1, row=1, sticky=(N, S))
         root.grid_columnconfigure(0, weight=1)
         root.grid_rowconfigure(0, weight=1)
-        
 
-        
+
         
         
         
