a8d4a766c84895eb011ce8bef6cd048d45c03966
degD <108623049+degD@users.noreply.github.com>
2022-07-28 18:24:15 +0300

    Update main.py
    Working on saving the canvas...

--- main.py
+++ main.py
@@ -14,23 +14,30 @@ class PixelPaintingCanvas(Canvas):
         self.bind("<Button-1>", self.paintsinglepixel)
         self.bind("<B1-Motion>", self.paintpixels)
         
+        self.data = []
+        # Find a way to speed this up.
+        
     def getcolor(self):
         return self.paintcolor
     
     def setcolor(self, color):
         self.paintcolor = color
+    
+    def savepixel(self, coords, color):
+        
+        self.data.append()
 
     def paintsinglepixel(self, event):
         x, y = self.canvasx(event.x), self.canvasy(event.y)
         
         rect_coords = self.pixelcalc(x, y)
-        self.create_rectangle(rect_coords, fill=self.paintcolor, outline=self.paintcolor)
+        self.create_rectangle(rect_coords, fill=self.paintcolor, outline='')
          
     def paintpixels(self, event):
         x, y = self.canvasx(event.x), self.canvasy(event.y)
         
         rect_coords = self.pixelcalc(x, y)
-        self.create_rectangle(rect_coords, fill=self.paintcolor, outline=self.paintcolor)
+        self.create_rectangle(rect_coords, fill=self.paintcolor, outline='')
         
     def pixelcalc(self, x, y):
         size = 20
@@ -41,7 +48,7 @@ class PixelPaintingCanvas(Canvas):
         topx, topy = px*size, py*size
         bottomx, bottomy = (px+1)*size, (py+1)*size
         
-        return (topx-2, topy-2, bottomx-2, bottomy-2)
+        return (topx, topy, bottomx, bottomy)
         
 
 class SizeDlg():
@@ -54,53 +61,58 @@ class SizeDlg():
             root.grab_release()
             root.destroy()
         
+        root.focus()
+        root.resizable(False, False)
         root.protocol('WM_DELETE_WINDOW', exitdlg)
         root.transient(subwindow)
         root.wait_visibility()
         root.grab_set()
         
         frame = ttk.Frame(root)
-        frame.grid(column=0, row=0, sticky=(N, S, W, E))
+        frame.grid(column=0, row=0, sticky=(N, S, W, E), padx=5, pady=5)
         
         ttk.Label(frame, text='Width:').grid(column=0, row=0, sticky=(N, S, W, E))
         ttk.Label(frame, text='Height:').grid(column=0, row=1, sticky=(N, S, W, E))
         
-        w = StringVar()
-        wentry = ttk.Entry(frame, width=5, textvariable=w)
-        wentry.grid(column=1, row=0, sticky=(N, S, W, E))
+        self.w = StringVar()
+        wentry = ttk.Entry(frame, width=5, textvariable=self.w)
+        wentry.grid(column=1, row=0, sticky=(N, S, W, E), pady=4)
         
-        h = StringVar()
-        hentry = ttk.Entry(frame, width=5, textvariable=h)
-        hentry.grid(column=1, row=1, sticky=(N, S, W, E))
+        self.h = StringVar()
+        hentry = ttk.Entry(frame, width=5, textvariable=self.h)
+        hentry.grid(column=1, row=1, sticky=(N, S, W, E), pady=4)
         
-        self.state = 0 
-        # 1 -> new win, 0 -> old win
+        self.newcanvas = False
         
         def setstate():
-            self.state = 1
+            self.newcanvas = True
             exitdlg()
         
         okbutton = ttk.Button(frame, text='Ok', command=setstate)
-        okbutton.grid(column=0, row=2, sticky=(N, S))
+        okbutton.grid(column=0, row=2, sticky=(N, S), padx=3, pady=3)
         
         cancelbutton = ttk.Button(frame, text='Cancel', command=exitdlg)
-        cancelbutton.grid(column=1, row=2, sticky=(N, S))
+        cancelbutton.grid(column=1, row=2, sticky=(N, S), padx=3, pady=3)
         
         root.wait_window()
         
     def getstate(self):
-        return self.state
+        return self.newcanvas
+    
+    def getsize(self):
+        return self.w.get(), self.h.get()
         
-        
-
 
 class PiePixelPainter():
     
     def __init__(self, root):
         self.root = root
         
-        root.title('PIE PIXEL PAINTER')
+        root.title('PPP')
         root.option_add('*tearOff', False)
+        
+        root.minsize(200, 200)
+        root.maxsize(1000, 800)
         
         menubar = Menu(root) # Menu bar
         root['menu'] = menubar
@@ -110,18 +122,20 @@ class PiePixelPainter():
         menu_recent = Menu(menu_file)
         menu_file.add_command(label='New', command=self.newcanvas)
         menu_file.add_separator()
-        menu_file.add_command(label='Exit')  
+        menu_file.add_command(label='Exit', command=root.destroy)  
 
         h_scroll = ttk.Scrollbar(root, orient=HORIZONTAL)
         v_scroll = ttk.Scrollbar(root, orient=VERTICAL)
         self.canvas = PixelPaintingCanvas(root, scrollregion=(0, 0, 1000, 1000), yscrollcommand=v_scroll.set, xscrollcommand=h_scroll.set, background='white')
+        self.canvas['width'] = 1000
+        self.canvas['height'] = 1000
         h_scroll['command'] = self.canvas.xview
         v_scroll['command'] = self.canvas.yview
         
-        self.canvas.grid(column=0, row=1, sticky=(N, S, W, E))
+        self.canvas.grid(column=0, row=1)
         h_scroll.grid(column=0, row=2, sticky=(W, E))
         v_scroll.grid(column=1, row=1, sticky=(N, S))
-       
+
         interface = ttk.Frame(root)
         interface.grid(column=0, row=0, sticky=(N, S, W, E))
         
@@ -146,18 +160,16 @@ class PiePixelPainter():
     def newcanvas(self):
         new = SizeDlg(self.root)
         state = new.getstate()
+        w, h = new.getsize()
         
-        if state == 1:
+        if state:
             self.canvas.delete('all')
             
-
-
-
-
-        
+            self.canvas['scrollregion'] = (0, 0, w, h)
+            self.canvas['width'] = w
+            self.canvas['height'] = h
+            
 
 root = Tk()
 PiePixelPainter(root)
-
-
 root.mainloop()
